f9b6f79f19d7bd76ac3cf82e98e04dd0d9052c69
osmanov <rhenium.osmium@gmail.com>
2018-02-24 23:30:21 +0300

    :hammer: rename src

---
+++ index.js
@@ -0,0 +1,65 @@
+class Observer {
+  constructor(subscribe) {
+    this._subscriber = subscribe
+  }
+  subscribe(observer) {
+    this._subscriber(observer)
+  }
+  static timer(time) {
+    return new Observer((observer) => {
+      const handler = setTimeout(() => {
+        observer.next()
+        observer.complete()
+      }, time)
+      return {
+        unsubscribe() {
+          clearTimeout(handler)
+        }
+      }
+    })
+  }
+  static fromEvent(el, evName) {
+    return new Observer(observer => {
+      const handler = (ev) => {
+        observer.next(ev)
+      }
+      el.addEventListener(evName, handler)
+      return {
+        unsubscribe() {
+          el.removeEventListener(evName, handler)
+        }
+      }
+    })
+  }
+
+  map(projection) {
+    return new Observer(observer => {
+      const subscription = this._subscriber({
+        next(ev) {
+          observer.next(projection(ev))
+        }
+      })
+      return subscription;
+
+    })
+  }
+}
+
+//HOW IT WORKS
+/*const time = Observer.timer(500)
+time.subscribe({
+    next() {
+        console.log('NEXT')
+    },
+    complete() {
+        console.log('COMPLETE')
+    }
+})
+
+const click$ = Observer.fromEvent(someDomEl, 'click');
+click$.map(client => client.offsetX).subscribe({
+    next(result) {
+        console.log(result)
+    }
+})
+*/
